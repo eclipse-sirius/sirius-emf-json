fb873b6510d152b839f34054bc233bfe02efa926
Vincent Richard <vincent.richard@obeo.fr>
2023-10-30 18:01:59 +0100

    [27] Add tests to validate that the attribute value can be changed
    Bug: https://github.com/eclipse-sirius/sirius-emf-json/issues/27

--- tests/org.eclipse.sirius.emfjson.tests/src/main/java/org/eclipse/sirius/emfjson/tests/internal/unit/load/JsonHelperDataLoadTests.java
+++ tests/org.eclipse.sirius.emfjson.tests/src/main/java/org/eclipse/sirius/emfjson/tests/internal/unit/load/JsonHelperDataLoadTests.java
@@ -72,4 +72,45 @@ public class JsonHelperDataLoadTests extends AbstractEMFJsonTests {
         this.testLoad("TestChangeAttributeTypeMulti.xmi"); //$NON-NLS-1$
     }
 
+    /**
+     * Change the value of a monovalued EAttribute.
+     */
+    @Test
+    public void testChangeAttributeValueMono() {
+        JsonHelper jsonHelper = new JsonHelper() {
+            @Override
+            public void setValue(EObject object, EStructuralFeature feature, Object value) {
+                Object newValue = value;
+                if ("NodeSingleValueAttribute".equals(feature.getEContainingClass().getName()) && "singleStringAttribute".equals(feature.getName())) { //$NON-NLS-1$ //$NON-NLS-2$
+                    newValue = ((String) value).toUpperCase();
+                }
+                super.setValue(object, feature, newValue);
+            }
+
+        };
+
+        this.options.put(JsonResource.OPTION_CUSTOM_HELPER, jsonHelper);
+        this.testLoad("TestChangeAttributeValueMono.xmi"); //$NON-NLS-1$
+    }
+
+    /**
+     * Change the value of a multivalued EAttribute.
+     */
+    @Test
+    public void testChangeAttributeValueMulti() {
+        JsonHelper jsonHelper = new JsonHelper() {
+            @Override
+            public void setValue(EObject object, EStructuralFeature feature, Object value) {
+                Object newValue = value;
+                if ("NodeMultiValuedAttribute".equals(feature.getEContainingClass().getName()) && "multiStringAttribute".equals(feature.getName())) { //$NON-NLS-1$ //$NON-NLS-2$
+                    newValue = ((String) value).toUpperCase();
+                }
+                super.setValue(object, feature, newValue);
+            }
+        };
+
+        this.options.put(JsonResource.OPTION_CUSTOM_HELPER, jsonHelper);
+        this.testLoad("TestChangeAttributeValueMulti.xmi"); //$NON-NLS-1$
+    }
+
 }
