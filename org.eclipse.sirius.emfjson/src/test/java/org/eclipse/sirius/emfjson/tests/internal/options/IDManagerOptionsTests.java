2357506175e3e8cd782b3bd845f0d903cf4c1a0e
Pierre-Charles David <pierre-charles.david@obeo.fr>
2025-04-16 09:37:52 +0200

    [cleanup] Fix CheckStyle warnings in the tests
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- org.eclipse.sirius.emfjson/src/test/java/org/eclipse/sirius/emfjson/tests/internal/options/IDManagerOptionsTests.java
+++ org.eclipse.sirius.emfjson/src/test/java/org/eclipse/sirius/emfjson/tests/internal/options/IDManagerOptionsTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Obeo.
+ * Copyright (c) 2020, 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -85,7 +85,7 @@ public class IDManagerOptionsTests extends AbstractEMFJsonTests {
      *
      * @author gcoutable
      */
-    private class TestIDManager implements IDManager {
+    private final class TestIDManager implements IDManager {
         /**
          * If ePackage ID has been properly retrieved.
          */
